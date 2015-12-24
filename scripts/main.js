f0d776dc86e060244373004de0a39d76d6ce1d91
Tianhao Li <ysihaoy@gmail.com>
2015-12-24 23:59:21 +1100

    Bi-Directional Data Flow with LinkState

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,10 @@ var createBrowserHistory = require('history/lib/createBrowserHistory')
 var Rebase = require('re-base');
 var base = Rebase.createClass('https://catch-of-the-day-t.firebaseio.com/');
 
+var Catalyst = require('react-catalyst');
+
 var App = React.createClass({
+  mixins : [Catalyst.LinkedStateMixin],
   getInitialState : function() {
     return {
       fishes : {},
@@ -68,7 +71,8 @@ var App = React.createClass({
           </ul>
         </div>
         <Order fishes={this.state.fishes} order={this.state.order} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}
+          fishes={this.state.fishes} linkState={this.linkState} />
       </div>
     )
   }
@@ -196,10 +200,27 @@ var Order = React.createClass({
 });
 
 var Inventory = React.createClass({
+  renderInventory : function(key) {
+    var linkState = this.props.linkState;
+    return (
+      <div className="fish-edit" key={key}>
+        <input type="text" valueLink={linkState('fishes.' + key + '.name')}/>
+        <input type="text" valueLink={linkState('fishes.' + key + '.price')}/>
+        <select valueLink={linkState('fishes.' + key + '.status')}>
+          <option value="unavailable">Sold Out!</option>
+          <option value="available">Fresh!</option>
+        </select>
+        <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
+        <input type="text" valueLink={linkState('fishes.' + key + '.image')}/>
+        <button>Remove Fish</button>
+      </div>
+    )
+  },
   render : function() {
     return (
       <div>
         <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm {...this.props} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
