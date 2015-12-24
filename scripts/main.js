951574482e1fdcca9ddff10a4b99669e9af30154
Tianhao Li <ysihaoy@gmail.com>
2015-12-24 23:16:12 +1100

    Setup Firebase

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,22 @@ var Navigation = ReactRouter.Navigation;
 var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory')
 
+// Firebase
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://catch-of-the-day-t.firebaseio.com/');
+
 var App = React.createClass({
   getInitialState : function() {
     return {
       fishes : {},
       order : {}
     }
+  },
+  componentDidMount : function() {
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context : this,
+      state : 'fishes'
+    });
   },
   addToOrder : function(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1
