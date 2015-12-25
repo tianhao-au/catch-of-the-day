e8e7f68e7f467e5536ef568c875350ac5408a625
Tianhao Li <ysihaoy@gmail.com>
2015-12-25 22:13:46 +1100

    Component Validation with PropTypes

--- scripts/main.js
+++ scripts/main.js
@@ -165,6 +165,9 @@ var Header = React.createClass({
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
+  },
+  propTypes : {
+    tagline : React.PropTypes.string.isRequired
   }
 });
 
@@ -223,6 +226,11 @@ var Order = React.createClass({
         </CSSTransitionGroup>
       </div>
     )
+  },
+  propTypes : {
+    fishes : React.PropTypes.object.isRequired,
+    order : React.PropTypes.object.isRequired,
+    removeFromOrder : React.PropTypes.func.isRequired
   }
 });
 
@@ -252,6 +260,13 @@ var Inventory = React.createClass({
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
+  },
+  propTypes : {
+    addFish : React.PropTypes.func.isRequired,
+    loadSamples : React.PropTypes.func.isRequired,
+    fishes : React.PropTypes.object.isRequired,
+    linkState : React.PropTypes.func.isRequired,
+    removeFish : React.PropTypes.func.isRequired
   }
 });
 
